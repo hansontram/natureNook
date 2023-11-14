80bee06a8ddc942afd02149fb85ea33834335030
Hanson Tram <htram.dev@gmail.com>
2023-11-14 12:03:53 -0700

    Implement all parks name to display

--- scripts/nationalParks.js
+++ scripts/nationalParks.js
@@ -1,17 +1,52 @@
-document.addEventListener("DOMContentLoaded", () =>{
-    
-    populateLocations(locationsArray)
-})
+document.addEventListener("DOMContentLoaded", () => {
+  populateLocations(locationsArray);
+  displayAllParks(nationalParksArray);
+});
 
-let populateLocations = (locationsArray) => {
-    const locationList = document.getElementById("locationList")
-    console.log(locationList, locationsArray)
-    // trigger function on change
-    // locationList.onchange = locationSelectionChanged
+const populateLocations = (locationsArray) => {
+  const locationList = document.getElementById("locationList");
 
-    for (let location of locationsArray) {
-        // creates new option element
-        const option = new Option(location);
-        locationList.appendChild(option);
-      }
-}
+  // trigger function on change
+  // locationList.onchange = locationSelectionChanged
+
+  for (let location of locationsArray) {
+    // creates new option element
+    const option = new Option(location);
+    locationList.appendChild(option);
+  }
+};
+
+const displayAllParks = (nationalParksArray) => {
+  const parksContainer = document.querySelector("#content");
+  // clear all elements
+  parksContainer.innerHTML = "";
+
+  nationalParksArray.forEach((park) => {
+    displayPark(park, parksContainer);
+    console.log(park.LocationName);
+  });
+};
+
+const displayPark = (park, parkDiv) => {
+  // Create a div for each park
+  const parkContainer = document.createElement("div");
+  parkContainer.classList.add("parkContainer");
+
+  parkDiv.appendChild(parkContainer);
+  // call functions to add details
+  addParkName(park, parkContainer);
+};
+
+// Functions to add park details
+
+const addParkName = (park, parkDiv) => {
+  // Create the park info div
+  const parkInfoContainer = document.createElement("div");
+  parkDiv.appendChild(parkInfoContainer);
+
+  // Add park name
+  const parkHeader = document.createElement("h4");
+  parkHeader.innerText = park.LocationName;
+
+  parkInfoContainer.appendChild(parkHeader)
+};
